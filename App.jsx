21f45bb8586b0ebea18487e3a2b604223176df1e
Jonatan Barr <jonybh99@gmail.com>
2019-10-06 13:10:52 +0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+class App extends React.Component {
+
+    render() {
+        return (
+            <main></main>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
